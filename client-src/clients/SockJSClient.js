a201923a3db9073484ed234ce3500a3075f669f2
yuang.zya <yuang.zya@alibaba-inc.com>
2020-05-12 12:02:26 +0800

    chore: cloudIDE场景替换hostname

--- client-src/clients/SockJSClient.js
+++ client-src/clients/SockJSClient.js
@@ -3,12 +3,18 @@
 /* eslint-disable
   no-unused-vars
 */
+const path = require('path')
 const SockJS = require('sockjs-client/dist/sockjs');
 const BaseClient = require('./BaseClient');
-
+const cwd = process.cwd()
+const config = require(path.join(cwd, './config'))
 module.exports = class SockJSClient extends BaseClient {
   constructor(url) {
     super();
+    if (config.cloudIDE) {
+      const hostName = process.env.HOSTNAME
+      url = `http://${hostName.split('-').slice(0, -2).join('-')}-8000.xide.aliyun.com/sockjs-node`
+    }
     this.sock = new SockJS(url);
 
     this.sock.onerror = (err) => {
